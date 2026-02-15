1e824b4f9413881dd6de3c223691f60e9d634de0
MuyaTheMman <themuyabros360@gmail.com>
2026-02-14 20:06:05 -0800

    Update en.php

--- en.php
+++ en.php
@@ -1,6 +1,6 @@
 <?php $lang = [
-    'title' => "YSOCRASS - The Crap of Future",
-    'comics' => 'COMICS',
-    'about' => 'ABOUT',
+    'title'=>"YSOCRASS - The Crap of Future",
+    'comics'=>'COMICS',
+    'about'=>'ABOUT',
 ];
 ?>
