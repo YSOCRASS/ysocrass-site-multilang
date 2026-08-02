b05119a3f3c4bf08c5cad9b197f56c19065f33e2
MuyaTheMman <themuyabros360@gmail.com>
2026-08-02 13:16:10 -0700

    d

---
+++ de.php
@@ -0,0 +1,23 @@
+<?php
+/*main menu*/
+$lang['test'] = "hello";
+$lang['maindesc'] = "Beware, your in for a scare. filled with cruel and graphical content from web!";
+$lang['movies'] = "MOVIES";
+$lang['novels'] = "NOVELS";
+$lang['comics'] = "COMICS";
+$lang['about'] = "ABOUT";
+
+/*about*/
+$lang['abouth1'] = "WATS A CRASS";
+$lang['abouth2'] = "Y SO CRASS";
+$lang['abouth3'] = "R U WOKE?";
+$lang['abouth4'] = "YSPCRASS FOR MINORS?";
+$lang['aboutp1'] = "YSOCRASS is multimedia productions founded by MuyaTheMman (aka me)";
+$lang['aboutp2'] = "YSC is gateaway for our opinions (is mostly me for now)";
+$lang['aboutp3'] = "no, the goals of YSC to make fun of everyone! maga, queers, pocs, etc";
+$lang['aboutp4'] = "despite our target audience filled of 10 years and grown adults, YSC is not for minors!";
+
+/*novels*/
+
+/*comics*/
+?>
